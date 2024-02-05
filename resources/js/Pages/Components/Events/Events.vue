ccac7323d3519fe9d19da4bfff9b77695621be0f
gightkingss <gightkingss@gmail.com>
2024-02-05 22:20:06 +0300

    Events

---
+++ resources/js/Pages/Components/Events/Events.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Event from './Event.vue';
+</script>
+
+<template>
+    <div class="flex flex-wrap mx-auto">
+        <Event></Event>
+        <Event></Event>
+        <Event></Event>
+        <Event></Event>
+    </div>
+</template>
---
+++ resources/js/Pages/Layouts/EventLayout.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import Events from '../Components/Events/Events.vue';
+
+</script>
+<template>
+    <div class="w-full">
+        <Events></Events>
+    </div>
+</template>
