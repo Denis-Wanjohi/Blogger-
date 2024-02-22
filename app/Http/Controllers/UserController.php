25989a9d14b34bdce3b7bf20603b8568546afd4f
gightkingss <gightkingss@gmail.com>
2024-02-22 12:37:24 +0300

    user Controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use League\MimeTypeDetection\FinfoMimeTypeDetector;
+
+class UserController extends Controller
+{
+    public function register(){
+        $user = request()->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required|min:8'
+        ]);
+     
+        $newUser = User::create($user);
+        Auth::login($newUser);
+        return redirect('/');
+
+    }
+
+    public function login(){
+        $details = request()->validate([
+            'password' => 'required',
+            'email' => 'required',
+        ]); 
+
+        if(Auth::attempt($details)){
+            request()->session()->regenerate();
+
+            return redirect('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'credentials doesnt match our records'
+        ])->onlyInput('email');
+    }
+
+    public function logout(){
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerate();
+    }
+}
