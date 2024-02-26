47d7359712c5f8c1f4e3b060fbf1a38f273578fd
gightkingss <gightkingss@gmail.com>
2024-02-26 16:38:25 +0300

    user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('faculty')->nullable();
             $table->string('profilePicture')->nullable();
+            $table->string('bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
