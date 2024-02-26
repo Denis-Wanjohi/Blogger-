55cb2c8eb0651cafaa52e411b0ef3207f85927c3
gightkingss <gightkingss@gmail.com>
2024-02-26 07:53:49 +0300

    Blog Collection  Resource

---
+++ app/Http/Resources/AllBlogsCollection.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class AllBlogsCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request)
+    {
+        // return parent::toArray($request);
+        return $this->collection->map(function ($blogs) {
+            return [
+                'id' => $blogs->id,
+                'user_id' => $blogs->user_id,
+                'Title' => $blogs->Title,
+                'mini_title' => $blogs->mini_title,
+                'description' => $blogs->description,
+                'banner' => $blogs->banner,
+                'faculty' => $blogs->faculty,
+                'created_at' => $blogs->created_at->format(' M D Y'),
+                'user' => [
+                    'id' => $blogs->user->id,
+                    'firstname' => $blogs->user->firstname,
+                    'lastname' => $blogs->user->lastname,
+                    'username' => $blogs->user->username,
+                    'faculty' => $blogs->user->faculty,
+                    'profilePicture' => $blogs->user->profilePicture 
+                ]
+            ];
+        });
+    }
+}
