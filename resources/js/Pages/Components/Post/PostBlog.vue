ddef58d47e1e76b7c5a8be4e0e73c55b255e2c87
gightkingss <gightkingss@gmail.com>
2024-03-09 23:50:28 +0300

    Blogger

--- resources/js/Pages/Components/Post/PostBlog.vue
+++ resources/js/Pages/Components/Post/PostBlog.vue
@@ -1,6 +1,9 @@
 <script setup>
 import HeadNav from '../../Shared/HeadNav.vue'
 import { router, useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+let clickable = ref(true)
 
 const form = useForm({
     title : null,
@@ -16,6 +19,7 @@ let submit = ()=>{
    })
 }
 
+
 </script>
 
 <template >
@@ -28,12 +32,12 @@ let submit = ()=>{
             <div class="flex sm:flex-row flex-col sm:w-3/4  w-full mx-auto justify-between">
                 <div class="flex flex-col sm:w-[70%] w-full  mx-auto ">
                     <label for="title" class="sm:text-start mb-2 ">Title</label>
-                    <input type="text" name="title" v-model="form.title" class="w-full  mx-auto min-h-[40px] pl-2 rounded-xl outline-none border-0 focus:shadow-blue-800 shadow-lg border-b">
+                    <input type="text" name="title" required v-model="form.title" class="w-full  mx-auto min-h-[40px] pl-2 rounded-xl outline-none border-0 focus:shadow-blue-800 shadow-lg border-b">
                     <p class="text-sm text-red-700">{{ $page.props.errors.title }}</p>
                 </div>
                 <div class="flex flex-col sm:w-[70%] sm:ml-3 w-full  mx-auto ">
                     <label for="mini_title" class="sm:text-start mb-2 ">Mini-Title</label>
-                    <input type="text" name="mini_title" v-model="form.mini_title" class="w-full  mx-auto min-h-[40px] pl-2 rounded-xl outline-none border-0 focus:shadow-blue-800 shadow-lg border-b">
+                    <input type="text" name="mini_title" required v-model="form.mini_title" class="w-full  mx-auto min-h-[40px] pl-2 rounded-xl outline-none border-0 focus:shadow-blue-800 shadow-lg border-b">
                     <p class="text-sm text-red-700">{{ $page.props.errors.mini_title }}</p>
                     
                 </div>
@@ -60,14 +64,18 @@ let submit = ()=>{
             </div>
             <div class="flex flex-col sm:w-3/4 w-full mx-auto">
                 <label for="description" class="sm:text-start mb-2 ">Description</label>
-                <textarea name="description" v-model="form.description" id="" cols="30" rows="10" class="border-2 border-black rounded-lg min-h-[200px] max-h-[250px]"></textarea>
+                <textarea name="description"  required v-model="form.description" id="" cols="30" rows="10" class="border-2 border-black rounded-lg min-h-[200px] max-h-[250px]"></textarea>
                 <p class="text-sm text-red-700">{{ $page.props.errors.description }}</p>
             </div>
             
             <div>
-                <button type="submit" class="bg-blue-500 font-bold mt-5 px-5 py-2 rounded-md hover:bg-blue-700 sm:w-1/2">
+                <button type="submit" class="bg-blue-500 font-bold mt-5 px-5 py-2 rounded-md hover:bg-blue-700 sm:w-1/2" v-if="clickable" @click.prevent="clickable = false">
                     POST
                 </button>
+                <!-- <p v-else  class="bg-blue-500 mx-auto font-bold mt-5 px-5 py-2 rounded-md hover:bg-blue-700 sm:w-1/2 cursor-wait" >
+                    POST
+                </p> -->
+
             </div>
         </form>
     </div>
